4e209e570f15d6b508b3ba1a37bbf1cf086579d2
Justin Maronde <myemailisjustin@me.com>
2024-09-03 08:24:22 -0400

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Justin Maronde on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
